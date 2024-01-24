d7f3c78650d121605c03e8210430a430b241a6f7
henrytrust1111 <henrytrust1111@gmail.com>
2024-01-25 00:15:12 +0100

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import './App.css'
-import Attendance from './Components/Attendance/Attendance'
-import Home from './Components/Home/home'
-import Payment from './Components/Payment/payment'
+import Attendance from './Components/Attendance/AttendancePage'
+import Home from './Components/Home/HomePage'
+import Payment from './Components/Payment/Payments'
 import SignUp from './Components/Sign/Signup'
-import Login from './Components/Login/login'
+import Login from './Components/Login/LoginPage'
 import Everybody from './Components/Auth/Everybody'
 import Ushers from './Components/Auth/Ushers'
 import Pastor_Admin from './Components/Auth/Pastor_Admin'
 import View_Payment from './Components/Payment/View_Payment'
 import View_Attendance from './Components/Attendance/View_Attendance'
 import Accountants_Pastor from './Components/Auth/Accountants_Pastor'
-import Manage from './Components/Manage/manage'
+import Manage from './Components/Manage/ManagePage'
 
 const router=createBrowserRouter([
   {
